07ec08935358367ff2464c594a326aa9227b62e7
Trista McCleary <trista_mccleary@yahoo.com>
2018-02-20 00:52:44 -0500

    add image upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Home Depot Project Tracker
+
+[Live Demo]( https://mccleary.github.io/Project-Tracker)
+
+
+This is a project I created to practice JavaScript and work on uploading images and text to a website.
+
+
+![Project Tracker](public/assets/img/home-depot-projects-tracker.png)
--- projects.js
+++ projects.js
@@ -1,17 +1,13 @@
-function previewFile(){
-  var preview = document.querySelector('img'); //selects the query named img
-  var file    = document.querySelector('input[type=file]').files[0]; //sames as here
-  var reader  = new FileReader();
+function previewFile() {
+  var preview = document.querySelector('img');
+  var file    = document.querySelector('input[type=file]').files[0];
+  var reader  = new FileReader();
 
-    reader.onloadend = function () {
-           preview.src = reader.result;
-    };
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
 
-    if (file) {
-        reader.readAsDataURL(file); //reads the data as a URL
-    } else {
-        preview.src = "";
-    }
+  if (file) {
+    reader.readAsDataURL(file);
+  }
 }
-
-previewFile();  //calls the function named previewFile()
